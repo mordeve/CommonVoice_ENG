0c3cdfdd880d883f481b473bdc8a24af8eeb117f
Batuhan Karaoglu <batuhan.karaoglu@ardictech.com>
2021-08-13 09:33:41 +0300

    preparing dataset adapted to work new dataset

--- prepare_dataset_common_voice.py
+++ prepare_dataset_common_voice.py
@@ -1,12 +1,10 @@
 import csv
 import os
-import numpy as np
 from os import path
 from pydub import AudioSegment
 from num2words import num2words as n2w
-import argparse
 import subprocess
-import glob
+import argparse
 
 parser = argparse.ArgumentParser(description="Preparing common voice dataset.")
 parser.add_argument('-d', '--dataset', type=str, help='which dataset to prepare for')
@@ -15,63 +13,62 @@ args = parser.parse_args()
 
 CHANNELS    = 1
 SAMPLE_RATE = 16000
-DATA_CSV    = f"cv-valid-{args.dataset}"
-
 err_num = 0
 
-def convert_to_wav(src, dst):
+def convert_to_wav_and_copy(src, dst, filename):
     global err_num
-    for sound in glob.glob(os.path.join(src, '*.mp3')):
-        # convert wav to mp3 
-        wav_name = sound.split("/")[-1].split('.mp3')[0] + ".wav"
-        try:   
-            dst_new = os.path.join(dst, wav_name)
-            subprocess.check_call(args=['ffmpeg', '-i','{}'.format(sound),\
-                 '-acodec','pcm_s16le','-ac', '{}'.format(CHANNELS), '-ar', '{}'.format(SAMPLE_RATE),'{}'.format(dst_new), '-y'])
+    filename_old = os.path.join(src, filename)
+    filename_new = filename.split('.mp3')[0] + ".wav"
+    try:   
+        dst_new = os.path.join(dst, filename_new)
+        subprocess.check_call(args=['ffmpeg', '-i','{}'.format(filename_old), \
+                '-acodec','pcm_s16le','-ac', '{}'.format(CHANNELS), '-ar',    \
+                    '{}'.format(SAMPLE_RATE),'{}'.format(dst_new), '-y'])
 
-        except Exception:
-            err_num += 1
-            print(f"An error occured in {src}, process is continuing.. TOTAL ERRORS:{err_num}")
-            continue
+    except Exception:
+        err_num += 1
+        print(f"An error occured in {src}, process is continuing.. TOTAL ERRORS:{err_num}")
 
-    print(f"All mp3 files converted to wav and copied to the wavs directory successfully.")
+    
+
+src_path      =  "./../../../../../Downloads/cv-corpus-eng/en/clips/"
+tsv_file_path = f"./../../../../../Downloads/cv-corpus-eng/en/{args.dataset}.tsv/"
+
+dst_path      = f"./common_voice_{args.dataset}/data/wavs/"
+csv_file_path = f"./common_voice_{args.dataset}/data/"
 
 
-src_path = f"./DataArchive/{DATA_CSV}/{DATA_CSV}"
-dst_path = f"./Ardıc_{args.dataset}_dataset/data/wavs/"
-csv_file_path = f"./Ardıc_{args.dataset}_dataset/data/"
 
 if not os.path.exists(dst_path):
-    print(f"{dst_path} is being created.")
+    print("dst path created.")
     os.makedirs(dst_path)
 
-convert_to_wav(src_path, dst_path)
+## read ./clips
+present_clips = os.listdir(dst_path)
+if ".DS_Store" in present_clips:
+    present_clips.remove(".DS_Store")
 
-# read wavs file
-present_clips = np.array(os.listdir(dst_path))
-if ".DS_Store" in present_clips: 
-    os.remove(".DS_Store")
-
-with open(f"./DataArchive/{DATA_CSV}.csv", "r") as csv_read_file: ## .tsv file
-    read_csv = csv.reader(csv_read_file)
-    with open(csv_file_path + "metadata.csv", 'w') as csv_write_file:
-        write_csv = csv.writer(csv_write_file)
-        for i, row in enumerate(read_csv):
+     
+with open("./LibriSpeech/validated.tsv", "r") as tsv_file: ## .tsv file
+    read_tsv = csv.reader(tsv_file, delimiter="\t")
+    with open(csv_file_path + 'metadata.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        for i, row in enumerate(read_tsv):
             if i == 0:
                 continue
-            if row[0].split("/")[-1].replace(".mp3", ".wav") not in present_clips: ## To avoid duplicate of data 
-                print("File {} is not in dataset.".format(row[0]))
-                continue
-            else:
-                id_to_write = row[0].split("/")[-1].split(".mp3")[0]
-                content = row[1].lower()
-                ## convert numbers to text
-                content_list = content.split()
-                for order, item in enumerate(content_list):
-                    if item.isdigit():
-                        item = (n2w(int(item), lang ='tr'))
-                        content_list[order] = item
-                content_clean = " ".join(content_list)
-                write_csv.writerow([id_to_write + "|" + content + "|" + content_clean])
+            if row[1] in present_clips: ## To avoid duplicate of data 
+               print("File {} is present in dataset.".format(row[1]))
+               continue
+            convert_to_wav_and_copy(src_path, dst_path, row[1])
+            content = row[2].lower()
+            ## convert numbers to text
+            content_list = content.split()
+            for order, item in enumerate(content_list):
+                if item.isdigit():
+                    item = (n2w(int(item), lang ='en'))
+                    content_list[order] = item
+            content_clean = " ".join(content_list)
+            writer.writerow([row[1].split(".mp3")[0] + "|" + content + "|" + content_clean])
 
+print("All mp3 files in dataset converted to wav and copied to the wavs directory successfully.")
 print("metadata.csv file is created successfully.")
